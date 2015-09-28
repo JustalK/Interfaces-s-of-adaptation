ed6ab1198e7ac4e8f7dd29b77a3bdd8ac5c081fd
Latsuj <justal.kevin@gmail.com>
2015-09-28 18:07:28 +0200

    [1] - Improvements of the structure
    Adding the LaTeX's presentation
    Starting the developpement

---
+++ Bootstrap/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+	include "constants.php";
+?>
+<!-- To ensure proper rendering and touch zooming for mobiles -->
+<!-- Desactivation of the zooming capabilities -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" charset="ISO-8859-1">
+<title><?php echo TITLE; ?></title>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- The CSS of the page -->
+<link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row fill">
+		<div id="colMovie" class="col-md-8">qsdqsd</div>
+		<div id="colPub" class="col-md-4">qsdqsd</div>
+	</div>
+</div>
+</body>
+</html>
