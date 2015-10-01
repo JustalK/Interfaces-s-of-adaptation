d44c88428be236ed3da9b47f09617e15c60081c3
Latsuj <justal.kevin@gmail.com>
2015-10-02 00:21:57 +0200

    [5] - Install Webcomponent & Polymer - Start the second website
    I have added some content in the presentation document. I have also
    installed all the web composant and the polymer's library on my local
    server. I have already started some implementation and test.

--- Bootstrap/index.php
+++ Bootstrap/index.php
@@ -22,27 +22,26 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <!-- The CSS of the page -->
 <link rel="stylesheet" href="style/main.css">
+<!-- The JS of the page -->
+<script src="js/main.js"></script>
 </head>
 <body>
-    <!-- Fixed navbar -->
+    <!-- MENU -->
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
       		<!-- Example row of columns -->
       		<div class="row">
         		<div class="col-xs-12 col-sm-12 col-md-12">
-       	          <div class="pull-left">
-        			<strong class="navbar-brand">Latsuj</strong>
-        		  </div>
-        		       	      <div class="pull-right navbar-brand">
-        		    	<button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#myModal">JOIN US!</button>
+       	        	<div class="pull-left">
+        				<strong class="navbar-brand">Latsuj</strong>
+        		  	</div>
+        		  	<div class="pull-right navbar-brand">
+        		  		<button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#myModal">JOIN US!</button>
        	          		<button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#myModal">LOGIN</button>
-					<br />
 					</div>
         		</div>
         	</div>
         </div>
-
-
     </nav>
     
     
@@ -78,13 +77,104 @@
 	</div>
 	</div>
   
-    <div class="container-full" style="background:#000000">
-	    <div class="container">
-	    <div class="row">
-	      <div class="col-md-12">
+  	<div id="elementSlide2" class="container-full" style="background:#000000">
+        		<div class="container">
+	    			<div class="row">
+	      				<div class="col-md-12">
+	          				<div class="embed-responsive embed-responsive-16by9" style="background:#000000">
+	    						<br />
+	    							<div style="background:#f8f8f8">
+	    							qsd
+	    							</div>
+	    						<br />
+			  				</div>
+	      				</div>
+					</div>
+	    		</div>
+  	</div>
+  
+    <div id="elementSlide" class="container-full" style="background:#000000">
+    
+    	<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+  
+  		<!-- Indicators -->
+  		<ol class="carousel-indicators">
+    		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    		<li data-target="#myCarousel" data-slide-to="1"></li>
+    		<li data-target="#myCarousel" data-slide-to="2"></li>
+    		<li data-target="#myCarousel" data-slide-to="3"></li>
+  		</ol>
+
+  		<!-- Wrapper for slides -->
+  		<div class="carousel-inner" role="listbox">
+  		
+    		<div class="item active">
+    			<div class="container">
+	    			<div class="row">
+	      				<div class="col-md-12">
+	          				<div class="embed-responsive embed-responsive-16by9">
+	    						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/RgKAFK5djSk"></iframe>
+			  				</div>
+	      				</div>
+					</div>
+	    		</div>
+    		</div>
+
+    		<div class="item">
+        		<div class="container">
+	    			<div class="row">
+	      				<div class="col-md-12">
+	          				<div class="embed-responsive embed-responsive-16by9">
+	    						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/j3CaHeakZF4"></iframe>
+			  				</div>
+	      				</div>
+					</div>
+	    		</div>
+    		</div>
+
+    		<div class="item">
+            	<div class="container">
+	    			<div class="row">
+	      				<div class="col-md-12">
+	          				<div class="embed-responsive embed-responsive-16by9">
+	    						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5dN3JV6sE9Q"></iframe>
+			  				</div>
+	      				</div>
+					</div>
+	    		</div>
+    		</div>
+
+    		<div class="item">
+            	<div class="container">
+	    			<div class="row">
+	      				<div class="col-md-12">
+	          				<div class="embed-responsive embed-responsive-16by9">
+	    						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YqeW9_5kURI"></iframe>
+			  				</div>
+	      				</div>
+					</div>
+	    		</div>
+    		</div>
+  		
+  		</div>
+  
+    	<!-- Left and right controls -->
+  		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    		<span class="sr-only">Previous</span>
+  		</a>
+  		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    		<span class="sr-only">Next</span>
+  		</a>
+
+	</div>
+	      	
+	      	<!-- 
 	          <div class="embed-responsive embed-responsive-16by9">
 	    		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/j3CaHeakZF4"></iframe>
 			  </div>
+			  -->
 	      </div>
 		</div>
 	    </div>
@@ -94,25 +184,25 @@
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-xs-push col-sm-3 col-md-3">
+        <div class="col-xs-push col-sm-6 col-md-6 col-lg-3">
           <h2><strong>Aon Luckkana</strong></h2>
           <div class="embed-responsive embed-responsive-16by9">
     		<iframe class="embed-responsive-item" src="http://www.viki.com/player/1035091v"></iframe>
 		  </div><br />
        	</div>
-        <div class="col-xs-push col-sm-3 col-md-3">
+        <div class="col-xs-push col-sm-6 col-md-6 col-lg-3">
           <h2><strong>My Jam</strong></h2>
           <div class="embed-responsive embed-responsive-16by9">
     		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5dN3JV6sE9Q"></iframe>
 		  </div><br />
        	</div>
-        <div class="col-xs-push col-sm-3 col-md-3">
+        <div class="col-xs-push col-sm-6 col-md-6 col-lg-3">
           <h2><strong>Five More Hours</strong></h2>
           <div class="embed-responsive embed-responsive-16by9">
     		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/j3CaHeakZF4"></iframe>
 		  </div><br />
        	</div>
-       	<div class="col-xs-push col-sm-3 col-md-3">
+       	<div class="col-xs-push col-sm-6 col-md-6 col-lg-3">
           <h2><strong>Nyan Nyan</strong></h2>
           <div class="embed-responsive embed-responsive-16by9">
     		<iframe class="embed-responsive-item" src="http://www.dailymotion.com/embed/video/xj20tk"></iframe>
---
+++ Bootstrap/js/main.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+	$('#elementSlide2').hide();
+});
