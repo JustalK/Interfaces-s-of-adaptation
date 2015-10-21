6bc3abb32a724c2ef7d0d68eb3ca14587c9ac3f0
Latsuj <justal.kevin@gmail.com>
2015-10-21 22:27:24 +0200

    [42] #29 - #35 - Change the name of the database

--- Bootstrap/inc/constants.php
+++ Bootstrap/inc/constants.php
@@ -4,7 +4,7 @@ define("TITLE","Bootstraps");
 /** ######################################################################################### **/
 /** CONFIGURATION SQL **/
 /** ######################################################################################### **/
-define("DATABASE_NAME","LATSUJ");
+define("DATABASE_NAME","latsu631031");
 define("MYSQL_HOST","localhost:3306");
 define("MYSQL_USER","root");
 define("MYSQL_PASSWORD","");
